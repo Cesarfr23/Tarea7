eacd1d993747820e7455e31c9da62e1387470499
Cesarfr23 <cesarfernandez860@gmail.com>
2016-07-02 17:02:20 +0100

    Agregando interfaz

---
+++ Tarea7/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Tarea7
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.NombreLabel = new System.Windows.Forms.Label();
+            this.ClaveLabel = new System.Windows.Forms.Label();
+            this.AgregarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // NombreLabel
+            // 
+            this.NombreLabel.AutoSize = true;
+            this.NombreLabel.Location = new System.Drawing.Point(40, 130);
+            this.NombreLabel.Name = "NombreLabel";
+            this.NombreLabel.Size = new System.Drawing.Size(44, 13);
+            this.NombreLabel.TabIndex = 2;
+            this.NombreLabel.Text = "Nombre";
+            // 
+            // ClaveLabel
+            // 
+            this.ClaveLabel.AutoSize = true;
+            this.ClaveLabel.Location = new System.Drawing.Point(43, 182);
+            this.ClaveLabel.Name = "ClaveLabel";
+            this.ClaveLabel.Size = new System.Drawing.Size(34, 13);
+            this.ClaveLabel.TabIndex = 3;
+            this.ClaveLabel.Text = "Clave";
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.Location = new System.Drawing.Point(46, 46);
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(75, 23);
+            this.AgregarButton.TabIndex = 4;
+            this.AgregarButton.Text = "Agregar";
+            this.AgregarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Location = new System.Drawing.Point(172, 46);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 23);
+            this.EliminarButton.TabIndex = 5;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(46, 229);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(414, 125);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 366);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.AgregarButton);
+            this.Controls.Add(this.ClaveLabel);
+            this.Controls.Add(this.NombreLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "Tarea 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label NombreLabel;
+        private System.Windows.Forms.Label ClaveLabel;
+        private System.Windows.Forms.Button AgregarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
